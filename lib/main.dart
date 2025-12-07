84edfdfaced829405f2c67e1c7f5d57b49385dfb
deadhack <developerrohan01@gmail.com>
2025-12-07 02:32:55 -0500

    Basic Setup for Auth is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'Features/Auth/Screens/auth.dart';
+import 'Features/Chat/Screens/chat.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
@@ -13,13 +17,54 @@ Future<void> main() async {
 }
 
 final supabase = Supabase.instance.client;
+class SupabaseAuthNotifier extends ChangeNotifier{
+  SupabaseAuthNotifier(){
+    supabase.auth.onAuthStateChange.listen((_){
+      notifyListeners();
+    });
+  }
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
   const MyApp({super.key});
   @override
-  Widget build (BuildContext context){
-    return MaterialApp(
+  State<MyApp> createState()=>_MyAppState();
 
+}
+class _MyAppState extends State<MyApp> {
+  late final GoRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _router = GoRouter(
+      initialLocation: '/chat',
+      refreshListenable: SupabaseAuthNotifier(),
+      routes: [
+        GoRoute(path: '/auth',
+          pageBuilder: (context, state) =>
+          const MaterialPage(child: AuthPage()),
+        ),
+        GoRoute(path: '/chat',
+          pageBuilder: (context, state) => MaterialPage(child: ChatPage()),)
+      ],
+      redirect: (context, state) {
+        final loggedIn = supabase.auth.currentSession != null;
+        final goingToAuth = state.matchedLocation == '/auth';
+        if (!loggedIn) return goingToAuth ? null : '/auth';
+        if (goingToAuth) return '/chat';
+        return null;
+      },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Supabase Based Chat Application',
+    );
+
   }
 }
